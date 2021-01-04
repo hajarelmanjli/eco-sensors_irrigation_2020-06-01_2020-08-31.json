ce20e53abc18275e42d9f564884c9f29e588fcef
hajarelmanjli <75116806+hajarelmanjli@users.noreply.github.com>
2021-01-04 10:11:39 +0100

    Update process_irrigation.py

--- process_irrigation.py
+++ process_irrigation.py
@@ -10,10 +10,3 @@ with open('eco-sensors_irrigation_2020-06-01_2020-08-31.json') as fichier:
 humidity_dataframe = pd.DataFrame(data_dict)
 humidity_dataframe
 
-
-
-def clean_data2(data):
-    for n in range(len(data)):
-         if data[n]==200:
-                data[n]=np.nan
-    return data
